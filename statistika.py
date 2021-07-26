064d2862b752ccb9099b5b78e61c7748d199908d
Luka <luka.horjak@student.fmf.uni-lj.si>
2021-07-26 21:05:31 +0200

    Dodano shranjevanje statistik in sprememba lestvic
    Lestvice sedaj prikažejo le po 10 najboljših v posamični kategoriji.
    Predolga imena se pri tem skrajšajo.

--- statistika.py
+++ statistika.py
@@ -1,9 +1,13 @@
 import json
 
-def get_stats():
+def get_stats() -> dict:
 	try:
 		with open("statistika.json") as datoteka:
 			slovar = json.load(datoteka)
 			return slovar
 	except FileNotFoundError:
 		return None
+
+def save_stats(slovar: dict) -> None:
+	with open("statistika.json", "w") as datoteka:
+		json.dump(slovar, datoteka, ensure_ascii=False, indent=4)
